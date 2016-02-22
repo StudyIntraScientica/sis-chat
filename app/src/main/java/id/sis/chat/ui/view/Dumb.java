ae6b04240ad4d588058aea49d847b6009f43e1c0
Zetra <zetra@mail.ugm.ac.id>
2016-02-22 19:17:06 +0700

    :v

---
+++ app/src/main/java/id/sis/chat/ui/view/Dumb.java
@@ -0,0 +1,12 @@
+package id.sis.chat.ui.view;
+
+/**
+ * Created on : February 22, 2016
+ * Author     : zetbaitsu
+ * Name       : Zetra
+ * Email      : zetra@mail.ugm.ac.id
+ * GitHub     : https://github.com/zetbaitsu
+ * LinkedIn   : https://id.linkedin.com/in/zetbaitsu
+ */
+public class Dumb {
+}
